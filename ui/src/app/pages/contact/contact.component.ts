88a07c38a113ea91f41cbc847a66ef43db4bee7a
mich.kolomanski <mich.kolomanski@gmail.com>
2024-12-10 17:10:46 +0100

    feat: prevent from resending the same message twice

--- ui/src/app/pages/contact/contact.component.ts
+++ ui/src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -12,14 +12,22 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
-  contactForm: FormGroup;
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+  lastSubmittedData: any = null;
+  isResendingBlocked: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       message: ['', Validators.required]
+    });
+
+    this.contactForm.valueChanges.subscribe(() => {
+      this.checkForResending();
     });
   }
 
@@ -29,7 +37,17 @@ export class ContactComponent {
 
   onSubmit() {
     if (this.contactForm.valid) {
+      const currentData = this.contactForm.value;
+      this.lastSubmittedData = currentData;
+      this.isResendingBlocked = true;
       alert('Thanks for contacting us! Message has been submitted.');
     }
+  }
+
+  checkForResending(): void {
+    const currentData = this.contactForm.value;
+    this.isResendingBlocked =
+      this.lastSubmittedData &&
+      JSON.stringify(this.lastSubmittedData) === JSON.stringify(currentData);
   }
 }
