3e7a4bf909c19eab5f7c9163cad258ed58705ad2
mich.kolomanski <mich.kolomanski@gmail.com>
2024-12-10 17:05:17 +0100

    feat: form validators

--- ui/src/app/pages/contact/contact.component.ts
+++ ui/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
+  imports: [
+    FormsModule, ReactiveFormsModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  get formControls() {
+    return this.contactForm.controls;
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      alert('Thanks for contacting us! Message has been submitted.');
+    }
+  }
 }
