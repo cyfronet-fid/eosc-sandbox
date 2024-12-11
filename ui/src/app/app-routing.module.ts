9e2e08c70e7c22be4e205bd8871907efe051b861
mich.kolomanski <mich.kolomanski@gmail.com>
2024-12-11 12:30:00 +0100

    fix: home page deleted

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
 import { FeaturesComponent } from './pages/features/features.component';
 import { PilotNodesComponent } from './pages/pilot-nodes/pilot-nodes.component';
 import { EndUsersComponent } from './pages/end-users/end-users.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AppComponent } from "./app.component";
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: AppComponent },
   { path: 'features', component: FeaturesComponent },
   { path: 'pilot-nodes', component: PilotNodesComponent },
   { path: 'end-users', component: EndUsersComponent },
