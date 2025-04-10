0b226344ae26a9d53aea35fb40b30d9935ad8141
Maria <maria77julia@gmail.com>
2025-04-10 15:32:01 +0200

    using APP_INITIALIZER

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { MainPageModule } from './pages/main-page/main-page.module';
@@ -17,23 +17,7 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'EOSC Sandbox';
-  zammadUrl: string = 'https://helpdesk.sandbox.eosc-beyond.eu/assets/form/form.js'
-
-  ngOnInit(): void {
-    this._loadScript(this.zammadUrl);
-  }
-
-  private _loadScript(url: string) {
-    const header = <HTMLHeadElement> document.head;
-    const script = document.createElement('script');
-    script.innerHTML = '';
-    script.src = url;
-    script.async = false;
-    script.defer = true;
-    script.id = 'zammad_form_script';
-    header.appendChild(script);
-  }
-
+  
 }
