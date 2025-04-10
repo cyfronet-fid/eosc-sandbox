7e5bac65da76350eae0e07462d649b6d9d04e738
Maria <maria77julia@gmail.com>
2025-04-10 17:28:21 +0200

    use APP_INITIALIZER

--- ui/src/app/app.config.ts
+++ ui/src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes'
+import { ScriptLoaderService } from './services/script-loader.service';
+
+export function loadJQuery(scriptLoader: ScriptLoaderService) {
+  const zammadUrl = 'https://helpdesk.sandbox.eosc-beyond.eu/assets/form/form.js'
+  return () => scriptLoader.loadScript(zammadUrl);
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadJQuery,
+      deps: [ScriptLoaderService],
+      multi: true
+    }
+
   ]
 };
